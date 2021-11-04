cd54481ad287b204557dd7a73d0fe79f20bda433
ssmathistad <ssmathistad@mail.csuchico.edu>
2021-11-04 12:28:26 -0700

    test: wait script for appium webdriver

--- wait-for-webdriver-agent.sh
+++ wait-for-webdriver-agent.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 while [ 1 ]
 do
-    pid=`ps -e | grep -i WebDriverAgentRunner-Runner | grep -v ttys`
-    echo $pid
-    if [ "$pid"!="" ]
+    curl  http://localhost:8100/status > temp.json
+    status=$(jq -r .value.state temp.json)
+    echo "${status}"
+    if [ "$status" = "success" ]
     then
         echo "Process has started."
         exit
